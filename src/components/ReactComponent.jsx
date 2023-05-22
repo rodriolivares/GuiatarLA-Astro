5c984f9989931f80f8e260c31e837ea6830655c3
rodriolivares <riolivares.dev@gmail.com>
2023-05-22 17:34:10 -0300

    first commit

---
+++ src/components/ReactComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ReactComponent = () => {
+   // console.log('desde el servidor');
+   // console.log('desde el cliente y el servidor');
+   console.log('desde el cliente');
+
+   return (
+      <div>React</div>
+   )
+}
+
+export default ReactComponent
